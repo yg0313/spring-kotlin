435deedeabbdddde79d1e5b7a8f3be8bb766baa2
Kim yonggwan <sssw0101@naver.com>
2022-04-30 15:39:43 +0900

    Initial commit

---
+++ src/main/kotlin/com/springkotlin/controller/BoardController.kt
@@ -0,0 +1,9 @@
+package com.springkotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class BoardController {
+
+}
